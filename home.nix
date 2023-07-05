0f98c12d407853a0d9a660a2e1b2f50b0a326592
Zarthoustra42 <simon.kenn.public@pm.me>
2023-07-05 22:17:48 +0200

    adding home.nix

---
+++ home.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "simon";
+  home.homeDirectory = "/home/simon";
+
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    htop
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
