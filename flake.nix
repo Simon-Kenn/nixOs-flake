cf36f560cc1ec500fbaa4be52ece49491979f8f7
Zarthoustra42 <simon.kenn.public@pm.me>
2023-07-05 22:24:26 +0200

    adding input for nixpkgs unstable, updating home-manager inputs

--- flake.nix
+++ flake.nix
@@ -3,12 +3,15 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; 
+
+
     home-manager = {
-	    url = github:nix-community/home-manager;
+	    url = github:nix-community/home-manager/release-23.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
     let
       user = "simon";
       system = "x86_64-linux";
@@ -25,7 +28,7 @@
             ./configuration.nix 
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
+              # home-manager.useUserPackages = true;
               home-manager.users.${user} = {
                 imports = [ ./home.nix ];
               };
