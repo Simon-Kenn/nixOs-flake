04c090176b0bdc44d61bebc02da77bd2a88318d2
Zarthoustra42 <simon.kenn.public@pm.me>
2023-07-17 23:55:48 +0200

    unify vim module

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -1,9 +1,18 @@
 { pkgs, lib, ... }:
+let 
+  plugins = pkgs.vimPlugins // pkgs.callPackage ./custom-plugins.nix {};
+in
 {
   programs = {
     vim = {
       enable = true;
+
+      plugins = with pkgs; [
+        plugins.catppuccin
+        nerdtree
+      ];
+
       extraConfig = builtins.readFile ./vimrc;
-    }; 
+    };
   };
 }
