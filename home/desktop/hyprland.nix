f0d265e335640c437e8180823f3ba5559532fb94
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-08 20:41:55 +0200

    better workspace and monitor workwflows

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -101,6 +101,13 @@
       # WIMDOWS RULES #
       # ------------- #
 
+      workspace = 1, monitor:${host.mainMonitor}, default:true
+      workspace = 2, monitor:${host.mainMonitor}, default:true
+      workspace = 3, monitor:${host.mainMonitor}, default:true
+      workspace = 4, monitor:${host.secondMonitor}, default:true
+      workspace = 5, monitor:${host.secondMonitor}, default:true
+      workspace = 6, monitor:${host.secondMonitor}, default:true
+
       # Floating
       # --------
 
@@ -161,13 +168,13 @@
       bind = $mainMod, quotedbl, workspace, 1
       bind = $mainMod, guillemotleft, workspace, 2
       bind = $mainMod, guillemotright, workspace, 3
-      bind = $mainMod, parenleft, workspace, 4
-      bind = $mainMod, parenright, workspace, 5
-      bind = $mainMod, at, workspace, 6
-      bind = $mainMod, plus, workspace, 7
-      bind = $mainMod, minus, workspace, 8
-      bind = $mainMod, slash, workspace, 9
-      bind = $mainMod, asterisk, workspace, 10
+      bind = $mainMod, parenleft, workspace, 7
+      bind = $mainMod, parenright, workspace, 8
+      bind = $mainMod, at, workspace, 9
+      bind = $mainMod, plus, workspace, 10
+      bind = $mainMod, minus, workspace, 4
+      bind = $mainMod, slash, workspace, 5
+      bind = $mainMod, asterisk, workspace, 6
 
 
       bind = $mainMod, right, workspace, +1
@@ -177,25 +184,25 @@
       bind = $mainMod SHIFT, quotedbl, movetoworkspace, 1
       bind = $mainMod SHIFT, guillemotleft, movetoworkspace, 2
       bind = $mainMod SHIFT, guillemotright, movetoworkspace, 3
-      bind = $mainMod SHIFT, parenleft, movetoworkspace, 4
-      bind = $mainMod SHIFT, parenright, movetoworkspace, 5
-      bind = $mainMod SHIFT, at, movetoworkspace, 6
-      bind = $mainMod SHIFT, plus, movetoworkspace, 7
-      bind = $mainMod SHIFT, minus, movetoworkspace, 8
-      bind = $mainMod SHIFT, slash, movetoworkspace, 9
-      bind = $mainMod SHIFT, asterisk, movetoworkspace, 10
+      bind = $mainMod SHIFT, parenleft, movetoworkspace, 7
+      bind = $mainMod SHIFT, parenright, movetoworkspace, 8
+      bind = $mainMod SHIFT, at, movetoworkspace, 9
+      bind = $mainMod SHIFT, plus, movetoworkspace, 10 
+      bind = $mainMod SHIFT, minus, movetoworkspace, 4
+      bind = $mainMod SHIFT, slash, movetoworkspace, 5
+      bind = $mainMod SHIFT, asterisk, movetoworkspace, 6
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
       bind = $mainMod SUPER_CTRL, quotedbl, movetoworkspacesilent, 1
       bind = $mainMod SUPER_CTRL, guillemotleft, movetoworkspacesilent, 2
       bind = $mainMod SUPER_CTRL, guillemotright, movetoworkspacesilent, 3
-      bind = $mainMod SUPER_CTRL, parenleft, movetoworkspacesilent, 4
-      bind = $mainMod SUPER_CTRL, parenright, movetoworkspacesilent, 5
-      bind = $mainMod SUPER_CTRL, at, movetoworkspacesilent, 6
-      bind = $mainMod SUPER_CTRL, plus, movetoworkspacesilent, 7
-      bind = $mainMod SUPER_CTRL, minus, movetoworkspacesilent, 8
-      bind = $mainMod SUPER_CTRL, slash, movetoworkspacesilent, 9
-      bind = $mainMod SUPER_CTRL, asterisk, movetoworkspacesilent, 10
+      bind = $mainMod SUPER_CTRL, parenleft, movetoworkspacesilent, 7
+      bind = $mainMod SUPER_CTRL, parenright, movetoworkspacesilent, 8
+      bind = $mainMod SUPER_CTRL, at, movetoworkspacesilent, 9
+      bind = $mainMod SUPER_CTRL, plus, movetoworkspacesilent, 10
+      bind = $mainMod SUPER_CTRL, minus, movetoworkspacesilent, 4
+      bind = $mainMod SUPER_CTRL, slash, movetoworkspacesilent, 5
+      bind = $mainMod SUPER_CTRL, asterisk, movetoworkspacesilent, 6
 
 
       # Scroll through existing workspaces with mainMod + scroll
