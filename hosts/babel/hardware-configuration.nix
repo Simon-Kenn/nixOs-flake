7800c6b5435964a2942377a44a8dc7ec46527e52
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-11 20:16:28 +0200

    Refactoring : less garbage

--- hosts/babel/hardware-configuration.nix
+++ hosts/babel/hardware-configuration.nix
@@ -1,17 +1,15 @@
 {
   config,
   lib,
-  modulesPath,
   ...
 }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["amdgpu"];
-  boot.extraModulePackages = with config.boot.kernelPackages; [];
+  boot = {
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+      kernelModules = ["amdgpu"];
+    };
+    kernelModules = ["amdgpu"];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXROOT";
@@ -25,21 +23,8 @@
 
   swapDevices = [];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault false;
   networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-
-  #networking = {
-  #  useDHCP = false;
-  #  hostName = "babel";
-  #  interfaces = {
-  #    enps50.useDHCP = lib.mkDefault true;
-  #  };
-  #};
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
