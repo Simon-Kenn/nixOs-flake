8029af6915103ef61e3d76e6bd716baa732332ab
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-12 10:01:29 +0200

    Architecture : git.nix -> cli/git.nix

---
+++ home/cli/git.nix
@@ -0,0 +1,48 @@
+{lib, ...}: {
+  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+    rm -f ~/.gitconfig
+  '';
+
+  programs.git = {
+    enable = true;
+
+    userName = "Zarthoustra42";
+    userEmail = "simon.kenn.public@pm.me";
+
+    extraConfig = {
+    };
+
+    aliases = {
+      a = "add";
+      b = "branch";
+      bc = "branch -c";
+      ch = "checkout";
+      c = "commit";
+      ca = "commit --amend";
+      can = "commit --amend --no-edit";
+      cl = "clone";
+      cm = "commit -m";
+      co = "checkout";
+      cp = "cherry-pick";
+      cpx = "cherry-pick -x";
+      d = "diff";
+      f = "fetch";
+      fo = "fetch origin";
+      fu = "fetch upstream";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      pl = "pull";
+      pr = "pull -r";
+      ps = "push";
+      psf = "push -f";
+      rb = "rebase";
+      rbi = "rebase -i";
+      r = "remote";
+      ra = "remote add";
+      rr = "remote rm";
+      rv = "remote -v";
+      rs = "remote show";
+      st = "status";
+    };
+  };
+}
