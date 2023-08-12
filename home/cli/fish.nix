994e23fc3b6931b27b6d5520de6d87490fef3aa5
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-12 13:01:47 +0200

    Feature : Fish -> add some aliases abrevation

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -2,6 +2,19 @@
   programs.fish = {
     enable = true;
 
+    shellAbbrs = {
+      vim = "nvim";
+      vi = "nvim";
+      v = "nvim";
+
+      cat = "bat";
+      fd = "find";
+      grep = "ripgrep";
+
+      ls = "exa";
+      ll = "exa -l";
+    };
+
     shellAliases = {
       nvim = "nix run ~/Projets/config/neovim --";
     };
