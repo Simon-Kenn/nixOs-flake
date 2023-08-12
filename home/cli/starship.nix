e1fcc296e384c8bb878181ffc584b6573922d373
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-12 10:49:58 +0200

    Feature : Starship -> adding username and hostname

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -4,5 +4,27 @@
 
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    settings = {
+      format = ''
+        $username$hostname
+      '';
+
+      fill = {
+        symbol = " ";
+        disabled = false;
+      };
+
+      username = {
+        format = "[$user]($style)";
+        show_always = true;
+      };
+
+      hostname = {
+        format = "[@$hostname]($style)";
+        ssh_only = false;
+        style = "bold green";
+      };
+    };
   };
 }
