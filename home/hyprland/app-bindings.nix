d0a8d044c6e86bd245ae389768b4653866566df9
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-13 12:44:38 +0200

    all app bindings in there own file

---
+++ home/hyprland/app-bindings.nix
@@ -0,0 +1,9 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    bind = [
+      "SUPER,Return,exec, kitty"
+      "SUPER,space,exec,wofi -S drun"
+      "SUPER,h,exec,wofi -S run"
+    ];
+  };
+}
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,32 +1,14 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+
   imports = [
     ./settings.nix
     ./workspaces.nix
     ./basic-bindings.nix
+    ./app-bindings.nix
     ./init-tty.nix
     ./startup-app.nix
   ];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-
-    settings = {
-      bind = let
-        wofi = "${config.programs.wofi.package}/bin/wofi";
-        terminal = config.home.sessionVariables.TERMINAL;
-      in [
-        "SUPER,Return,exec,${terminal}"
-        "SUPER,space,exec,${wofi} -S drun"
-        "SUPER,h,exec,${wofi} -S run"
-      ];
-    };
-    extraConfig = ''
-            # exec-once = waybar & logseq & kitty & firefox & mako
-      #       exec=swaybg -m fill -i ~/.local/share/wallpapers/abstract_ghost.jpg
-    '';
-  };
 }
