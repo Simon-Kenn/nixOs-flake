1fca832fcb9515fd9c732c9e05242c1e6a6b9746
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-13 12:40:23 +0200

    Delete useless stuff

--- home/hyprland/startup-app.nix
+++ home/hyprland/startup-app.nix
@@ -7,11 +7,8 @@
     exec = [
       "${pkgs.swaybg}/bin/swaybg -i ${config.wallpaper} --mode fill"
     ];
-    exec-once = let
-      waybar = "${config.programs.waybar.package}/bin/waybar";
-      mako = "${config.services.mako.package}/bin/mako";
-    in [
-      "${waybar} & ${mako}"
+    exec-once = [
+      "waybar & mako"
     ];
   };
 }
