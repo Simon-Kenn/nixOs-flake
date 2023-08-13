d638550bd4afce90cdd0b7edde14cbeb25cbe7d7
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-13 12:35:47 +0200

    fix mako and update it

--- home/wizard-tools/mako.nix
+++ home/wizard-tools/mako.nix
@@ -3,25 +3,22 @@
 in {
   services.mako = {
     enable = true;
-
+    iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
+    font = "${config.fontProfiles.regular.family} 12";
+    padding = "10, 20";
     anchor = "top-right";
-    font = "${config.fontProfiles.regular.family} 11";
-    backgroundColor = "#${colors.base00}dd";
-    borderColor = "#${colors.base0D}";
-    textColor = "#${colors.base05}dd";
     width = 400;
-    height = 100;
-    margin = "20";
-    padding = "7, 12, 10";
-    borderSize = 1;
-    borderRadius = 3;
-    progressColor = "over #5588AAFF";
-    icons = true;
-    maxIconSize = 32;
-    markup = true;
-    actions = true;
-    format = "<b>%s</b>\n%b";
-    defaultTimeout = 5000;
-    ignoreTimeout = false;
+    height = 150;
+    borderSize = 2;
+    defaultTimeout = 12000;
+    borderRadius = 5;
+    backgroundColor = "#${colors.base00}dd";
+    borderColor = "#${colors.base03}dd";
+    textColor = "#${colors.base05}dd";
+
+    extraConfig = ''
+      [urgency=high]
+      border-color=#${colors.base08}dd
+    '';
   };
 }
