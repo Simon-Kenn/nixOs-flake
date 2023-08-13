b54b26d022336c89ea2794921045963db3221c6f
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-13 10:28:22 +0200

    mooving every basic bindings to it's own file

---
+++ home/hyprland/basic-bindings.nix
@@ -0,0 +1,77 @@
+let
+  main = "SUPER";
+  secondMain = "SUPER SHIFT";
+  alt = "ALT";
+  secondAlt = "SUPER ALT";
+in {
+  wayland.windowManager.hyprland.settings = {
+    bindm = [
+      "${main}, mouse:272,            movewindow"
+      "${main}, mouse:273,            resizewindow"
+    ];
+    bind = [
+      # window operations
+      "${secondMain},q,               killactive"
+      "${secondMain},c,               exit"
+      "${main}, v,                    togglefloating"
+      "${main}, p,                    pseudo"
+      "${main}, j,                    togglesplit"
+      "${main}, f,                    fullscreen"
+
+      # change focus
+      "${main}, t,                    movefocus, l"
+      "${main}, n,                    movefocus, r"
+      "${main}, r,                    movefocus, u"
+      "${main}, s,                    movefocus, d"
+
+      # move window
+      "${alt}, t,                     movewindow, l"
+      "${alt}, n,                     movewindow, r"
+      "${alt}, r,                     movewindow, u"
+      "${alt}, s,                     movewindow, d"
+
+      # change workspace
+      "${main}, quotedbl,             workspace, 1"
+      "${main}, guillemotleft,        workspace, 2"
+      "${main}, guillemotright,       workspace, 3"
+      "${main}, parenleft,            workspace, 4"
+      "${main}, parenright,           workspace, 5"
+      "${main}, at,                   workspace, 6"
+      "${main}, plus,                 workspace, 7"
+      "${main}, minus,                workspace, 8"
+      "${main}, slash,                workspace, 9"
+      "${main}, asterisk,             workspace, 10"
+
+      # move active windowe to workspace x
+      "${secondMain}, quotedbl,       movetoworkspace, 1"
+      "${secondMain}, guillemotleft,  movetoworkspace, 2"
+      "${secondMain}, guillemotright, movetoworkspace, 3"
+      "${secondMain}, parenleft,      movetoworkspace, 4"
+      "${secondMain}, parenright,     movetoworkspace, 5"
+      "${secondMain}, at,             movetoworkspace, 6"
+      "${secondMain}, plus,           movetoworkspace, 7"
+      "${secondMain}, minus,          movetoworkspace, 8"
+      "${secondMain}, slash,          movetoworkspace, 9"
+      "${secondMain}, asterisk,       movetoworkspace, 10"
+
+      # move active windowe to workspace x in silence
+      "${secondAlt}, quotedbl,       movetoworkspacesilent, 1"
+      "${secondAlt}, guillemotleft,  movetoworkspacesilent, 2"
+      "${secondAlt}, guillemotright, movetoworkspacesilent, 3"
+      "${secondAlt}, parenleft,      movetoworkspacesilent, 4"
+      "${secondAlt}, parenright,     movetoworkspacesilent, 5"
+      "${secondAlt}, at,             movetoworkspacesilent, 6"
+      "${secondAlt}, plus,           movetoworkspacesilent, 7"
+      "${secondAlt}, minus,          movetoworkspacesilent, 8"
+      "${secondAlt}, slash,          movetoworkspacesilent, 9"
+      "${secondAlt}, asterisk,       movetoworkspacesilent, 10"
+
+      # Scrolling through active workspace
+      "${main}, right,            workspace, +1"
+      "${main}, left,             workspace, -1"
+
+      "${main}, mouse_down,       workspace, e+1"
+      "${main}, mouse_up,         workspace, e-1"
+    ];
+  };
+}
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,7 +1,12 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./settings.nix
     ./workspaces.nix
+    ./basic-bindings.nix
   ];
   wayland.windowManager.hyprland = {
     enable = true;
@@ -22,86 +27,6 @@
     extraConfig = ''
       exec-once = waybar & logseq & kitty & firefox & mako
       exec=swaybg -m fill -i ~/.local/share/wallpapers/abstract_ghost.jpg
-
-
-      # BINDINGS
-      # --------
-      $mainMod = SUPER
-
-      # Exec
-      # bind = $mainMod, RETURN, exec, kitty
-      # bind = $mainMod, SPACE, exec, wofi --show drun
-      bind = $mainMod, L, exec, swaylock --grace 0 --fade-in 0
-
-
-      # Window operation
-      bind = $mainMod, C, killactive,
-      bind = $mainMod, M, exit,
-      bind = $mainMod, V, togglefloating,
-      bind = $mainMod, P, pseudo, # dwindle
-      bind = $mainMod, J, togglesplit, # dwindle
-      bind = $mainMod, F, fullscreen
-
-      # WINDOWS FOCUS
-      bind = $mainMod, t, movefocus, l
-      bind = $mainMod, n, movefocus, r
-      bind = $mainMod, r, movefocus, u
-      bind = $mainMod, s, movefocus, d
-
-      bind = ALT, t, movewindow, l
-      bind = ALT, n, movewindow, r
-      bind = ALT, r, movewindow, u
-      bind = ALT, s, movewindow, d
-
-
-      # Switch workspaces with mainMod + [0-9]
-      bind = $mainMod, quotedbl, workspace, 1
-      bind = $mainMod, guillemotleft, workspace, 2
-      bind = $mainMod, guillemotright, workspace, 3
-      bind = $mainMod, parenleft, workspace, 7
-      bind = $mainMod, parenright, workspace, 8
-      bind = $mainMod, at, workspace, 9
-      bind = $mainMod, plus, workspace, 10
-      bind = $mainMod, minus, workspace, 4
-      bind = $mainMod, slash, workspace, 5
-      bind = $mainMod, asterisk, workspace, 6
-
-
-      bind = $mainMod, right, workspace, +1
-      bind = $mainMod, left, workspace, -1
-
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      bind = $mainMod SHIFT, quotedbl, movetoworkspace, 1
-      bind = $mainMod SHIFT, guillemotleft, movetoworkspace, 2
-      bind = $mainMod SHIFT, guillemotright, movetoworkspace, 3
-      bind = $mainMod SHIFT, parenleft, movetoworkspace, 7
-      bind = $mainMod SHIFT, parenright, movetoworkspace, 8
-      bind = $mainMod SHIFT, at, movetoworkspace, 9
-      bind = $mainMod SHIFT, plus, movetoworkspace, 10
-      bind = $mainMod SHIFT, minus, movetoworkspace, 4
-      bind = $mainMod SHIFT, slash, movetoworkspace, 5
-      bind = $mainMod SHIFT, asterisk, movetoworkspace, 6
-
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      bind = $mainMod SUPER_CTRL, quotedbl, movetoworkspacesilent, 1
-      bind = $mainMod SUPER_CTRL, guillemotleft, movetoworkspacesilent, 2
-      bind = $mainMod SUPER_CTRL, guillemotright, movetoworkspacesilent, 3
-      bind = $mainMod SUPER_CTRL, parenleft, movetoworkspacesilent, 7
-      bind = $mainMod SUPER_CTRL, parenright, movetoworkspacesilent, 8
-      bind = $mainMod SUPER_CTRL, at, movetoworkspacesilent, 9
-      bind = $mainMod SUPER_CTRL, plus, movetoworkspacesilent, 10
-      bind = $mainMod SUPER_CTRL, minus, movetoworkspacesilent, 4
-      bind = $mainMod SUPER_CTRL, slash, movetoworkspacesilent, 5
-      bind = $mainMod SUPER_CTRL, asterisk, movetoworkspacesilent, 6
-
-
-      # Scroll through existing workspaces with mainMod + scroll
-      bind = $mainMod, mouse_down, workspace, e+1
-      bind = $mainMod, mouse_up, workspace, e-1
-
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod, mouse:273, resizewindow
     '';
   };
 }
