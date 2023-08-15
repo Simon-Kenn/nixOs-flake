4cd7eefc5d3bfc005a93eafc55a91d3a1f91f259
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-15 09:29:13 +0200

    add inputs neorg-overlay

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-colors.url = "github:misterio77/nix-colors";
+    neorg-overlay.url = "github:nvim-neorg/nixpkgs-neorg-overlay";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = inputs @ {
@@ -32,7 +32,9 @@
     };
 
     lib = nixpkgs.lib;
+    neorg-overlay = neorg-overlay.overlays.default;
   in {
+    nixpkgs.overlays = [ neorg-overlay];
     homeManagerModules = import ./modules/home-manager;
 
     nixosConfigurations = {
