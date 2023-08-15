010e3689ea90128d800975ff3c41f4bb758e3bfc
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-15 13:20:42 +0200

    telescope add shortcut

--- home/nixvim/modules/telescope.nix
+++ home/nixvim/modules/telescope.nix
@@ -1,5 +1,14 @@
 {
-  programs.nixvim.programs.telescope = {
+  programs.nixvim.plugins.telescope = {
     enable = true;
+
+    keymaps = {
+      "<leader>ff" = "find_files";
+      "<leader>fg" = "live_grep";
+      "<leader>fb" = "buffers";
+      "<leader>fh" = "help_tags";
+
+      "<leader>fs" = "treesitter";
+    };
   };
 }
