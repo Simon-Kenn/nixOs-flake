1d2f80f8fb441257d2ecdf88bec9f28e3e72918e
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-15 09:42:41 +0200

    nvim : adding neorg

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,13 +1,37 @@
-{ config, pkgs, ...}:
-let
-  color = pkgs.writeText "color.vim" (import ./theme.nix config.colosheme);
-in
 {
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [];
 
   programs.neovim = {
     enable = true;
 
+    plugins = with pkgs.vimPlugins; [
+      neorg
+      neorg-telescope
+      (nvim-treesitter.withPlugins (p:
+        with p; [
+          tree-sitter-lua
+        ]))
+    ];
 
+    extraConfig = builtins.readFile(./vimrc);
+
+    extraLuaConfig = ''
+      require("nvim-treesitter.configs").setup {
+        highlight = {
+          enable = true,
+        }
+      }
+
+
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}
+        }
+      }
+    '';
   };
 }
