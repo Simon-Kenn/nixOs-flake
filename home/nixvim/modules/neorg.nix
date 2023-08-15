50c5413483f139c3fbb813db5555be58f3164309
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-15 19:25:59 +0200

    add completion

--- home/nixvim/modules/neorg.nix
+++ home/nixvim/modules/neorg.nix
@@ -3,12 +3,24 @@
     enable = true;
     modules = {
       "core.defaults" = {__empty = null;};
-      "core.concealer" = {__empty = null;};
+      "core.keybinds" = {
+        config = {
+        };
+      };
+
+      "core.completion" = {
+        config = {
+          engine = "nvim-cmp";
+        };
+      };
+
+      "core.concealer" = {};
       "core.dirman" = {
         config = {
           workspaces = {
             notes = "~/notes";
           };
+          default_workspace = "notes";
         };
       };
     };
