bdd38a716721339137781d5ac9cbcdb66b56ef53
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-19 08:56:26 +0200

    nvim-cmp: shortcut around tab

--- home/nixvim/modules/ux/nvim-cmp.nix
+++ home/nixvim/modules/ux/nvim-cmp.nix
@@ -1,32 +1,39 @@
 {
   programs.nixvim.plugins.nvim-cmp = {
     enable = true;
-		
-		sources = [
-			{ name = "buffer"; }
-			{ name = "path"; }
-		];
 
-		mapping = {
-			"<CR>" = "cmp.mapping.confirm({ select = true })";
-			"<Tab>" = {
-				modes = ["i" "s" ];
-				action = ''
-					function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expandable() then
-							luasnip.expand()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jumb()
-						elseif check_backspace() then
-							fallback()
-						else
-							fallback()
-						end
-					end
-				'';
+    sources = [
+      {name = "buffer";}
+      {name = "path";}
+    ];
+    mapping = {
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+      "C-Space" = {
+				modes = ["i" "c"]; 
+				action = "cmp.mapping.complete({select = true})";
 			};
-		};
+      "<Tab>" = {
+        action = ''
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end
+        '';
+      };
+			"<S-Tab>" = {
+        action = ''
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end
+        '';
+      };
+    };
   };
 }
