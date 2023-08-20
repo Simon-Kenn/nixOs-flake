3959d779450351bc659262e60bae39c1a4e33f75
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-20 04:34:10 +0200

    using catppuccin everywhere

--- home/nixvim/modules/ui/colorsheme.nix
+++ home/nixvim/modules/ui/colorsheme.nix
@@ -2,5 +2,11 @@
 	programs.nixvim.colorschemes.catppuccin = {
     enable = true;
     flavour = "frappe";
+		integrations = {
+			cmp = true;
+			treesitter = true;
+			telescope = true;
+			which_key = true;
+		};
 	};
  }
