c50d40523b586be1372401940736435370a69de3
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-21 19:12:17 +0200

    Neorg keybinds adding hop and new note

--- home/nixvim/modules/neorg/core/keybinds.nix
+++ home/nixvim/modules/neorg/core/keybinds.nix
@@ -3,8 +3,11 @@
 		default_keybinds = false;
 		hook = { __raw = let 
 			todo = "<cmd>Neorg keybind norg core.qol.todo_items.todo.task_";
+			dirman = "<cmd>Neorg keybind norg core.dirman";
+			hop = "<cmd>Neorg keybind norg core.esupports.hop";
 			in ''
 				function(keybinds)
+						-- qol.todo_items
 						keybinds.map("norg",	"n", "<LocalLeader>tu", "${todo}undone<CR>")
 						keybinds.map("norg",	"n", "<LocalLeader>tp", "${todo}pending<CR>")
 						keybinds.map("norg",	"n", "<LocalLeader>td", "${todo}done<CR>")
@@ -14,6 +17,17 @@
 						keybinds.map("norg",	"n", "<LocalLeader>ti", "${todo}important<CR>")
 						keybinds.map("norg",	"n", "<LocalLeader>ta", "${todo}ambiguous<CR>")
 						keybinds.map("norg",	"n", "<LocalLeader>ts", "${todo}cycle<CR>")
+						keybinds.map("norg",	"n", "<LocalLeader>tsr", "${todo}cycle_reverse<CR>")
+
+						-- dirman
+						keybinds.map("norg",	"n", "<LocalLeader>nn", "${dirman}.new.note<CR>")
+
+						-- hop
+						keybinds.map("norg",	"n", "<CR>", "${hop}.hop-link<CR>")
+						keybinds.map("norg",	"n", "<M-CR>", "${hop}.hop-link vsplit<CR>")
+						keybinds.map("norg",	"n", "<LocalLeader>gd", "${hop}.hop-link<CR>")
+						keybinds.map("norg",	"n", "<LocalLeader>gh", "${hop}.hop-link split<CR>")
+						keybinds.map("norg",	"n", "<LocalLeader>gg", "${hop}.hop-link vsplit<CR>")
 				end
 			'';};
 	};
