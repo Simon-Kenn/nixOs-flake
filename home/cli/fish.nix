7c5bf282bf1e646f3e648f45de2736fc3bdc5e58
Zarthoustra42 <simon.kenn.public@pm.me>
2023-08-29 16:17:39 +0200

    Use terminal colors

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -25,33 +25,33 @@ in {
 			fish_greeting = "";
 		};
 		interactiveShellInit = ''
-			set -U fish_color_normal cdd6f4
-			set -U fish_color_command 89b4fa
-			set -U fish_color_param f2cdcd
-			set -U fish_color_keyword f38ba8
-			set -U fish_color_quote a6e3a1
-			set -U fish_color_redirection f5c2e7
-			set -U fish_color_end fab387
-			set -U fish_color_comment 7f849c
-			set -U fish_color_error f38ba8
-			set -U fish_color_gray 6c7086
-			set -U fish_color_selection --background=313244
-			set -U fish_color_search_match --background=313244
-			set -U fish_color_option a6e3a1
-			set -U fish_color_operator f5c2e7
-			set -U fish_color_escape eba0ac
-			set -U fish_color_autosuggestion 6c7086
-			set -U fish_color_cancel f38ba8
-			set -U fish_color_cwd f9e2af
-			set -U fish_color_user 94e2d5
-			set -U fish_color_host 89b4fa
-			set -U fish_color_host_remote a6e3a1
-			set -U fish_color_status f38ba8
-			set -U fish_pager_color_progress 6c7086
-			set -U fish_pager_color_prefix f5c2e7
-			set -U fish_pager_color_completion cdd6f4
-			set -U fish_pager_color_description 6c7086
-		'' + 
+      set -U fish_color_autosuggestion      brblack
+      set -U fish_color_cancel              -r
+      set -U fish_color_command             brgreen
+      set -U fish_color_comment             brmagenta
+      set -U fish_color_cwd                 green
+      set -U fish_color_cwd_root            red
+      set -U fish_color_end                 brmagenta
+      set -U fish_color_error               brred
+      set -U fish_color_escape              brcyan
+      set -U fish_color_history_current     --bold
+      set -U fish_color_host                normal
+      set -U fish_color_match               --background=brblue
+      set -U fish_color_normal              normal
+      set -U fish_color_operator            cyan
+      set -U fish_color_param               brblue
+      set -U fish_color_quote               yellow
+      set -U fish_color_redirection         bryellow
+      set -U fish_color_search_match        'bryellow' '--background=brblack'
+      set -U fish_color_selection           'white' '--bold' '--background=brblack'
+      set -U fish_color_status              red
+      set -U fish_color_user                brgreen
+      set -U fish_color_valid_path          --underline
+      set -U fish_pager_color_completion    normal
+      set -U fish_pager_color_description   yellow
+      set -U fish_pager_color_prefix        'white' '--bold' '--underline'
+      set -U fish_pager_color_progress      'brwhite' '--background=cyan'
+		'' +
 		''
 			#set -l rebind true
 			#bind --erase --all --preset
@@ -62,8 +62,6 @@ in {
 			## Fin de ligne / Début de ligne
 			#set -l eol_keys \$ g\$ \u00E7 # $ g$ ç
 			#set -l bol_keys \^ 0 g\^ \u00E8 # ^ 0 g^ è
-
-			
 		'';
   };
 }
