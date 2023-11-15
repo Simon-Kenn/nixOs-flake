cc0eddbabc7b7f821a166229c1eafc10ec01fbf3
Zarthoustra42 <simon.kenn.public@pm.me>
2023-11-15 17:37:49 +0100

    fix gpg.nix

--- home/cli/gpg.nix
+++ home/cli/gpg.nix
@@ -1,5 +1,21 @@
-{
-	programs.gpg = {
+{pkgs, ...}: {
+	home.packages = [ pkgs.pinentry-curses ];
+
+	services.gpg-agent = {
 		enable = true;
+		pinentryFlavor = "curses";
+	};
+
+	programs = 
+	let 
+		fixGpg = /* bash */ ''
+			gpgconf --launch gpg-agent;
+		'';
+	in
+	{
+		fish.loginShellInit = fixGpg;
+		gpg = {
+			enable = true;
+		};
 	};
 }
