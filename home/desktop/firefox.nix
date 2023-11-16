8e698c0e39c349328dbcf17c0094796e92f38910
Zarthoustra42 <simon.kenn.public@pm.me>
2023-11-16 17:49:14 +0100

    enhence firfox settings

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -2,6 +2,54 @@
 	home.sessionVariables.BROWSER = "firefox";
   programs.firefox = {
     enable = true;
+		profiles.simon = {
+			bookmarks = { };
+			#extensions = with pkgs.inputs.firefox-addons; [
+			#	ublock-origin
+			##	browserpass
+		#	#	bitwarden
+			#];
+			settings = {
+				"browser.startup.homepage" = "https://start.duckduckgo.com";
+				"browser.contentblockings.category" = true;
+        "app.shield.optoutstudies.enabled" = false;
+        "app.update.auto" = false;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.contentblocking.category" = "strict";
+        "browser.ctrlTab.recentlyUsedOrder" = true;
+        "browser.discovery.enabled" = false;
+        "browser.laterrun.enabled" = false;
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" =
+          false;
+        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" =
+          false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
+        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" =
+          false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.pinned" = false;
+        "browser.protections_panel.infoMessage.seen" = true;
+        "browser.quitShortcut.disabled" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.ssb.enabled" = true;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.urlbar.suggest.openpage" = false;
+        "datareporting.policy.dataSubmissionEnable" = false;
+        "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "extensions.pocket.enabled" = false;
+        "identity.fxaccounts.enabled" = false;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+			};
+		};
   };
 
   xdg.mimeApps.defaultApplications = {
